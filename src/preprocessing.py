21d59bf321d1e532af0be6f7ac10d8f5705ea45e
Sahil Joshi <sail.joshi0604@gmail.com>
2026-06-03 22:11:40 +0530

    model created

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # This dataset does not need any preprocessing steps to be done 
-# Also Encoding and Scaling is done in model pipline
+# Also Encoding and Scaling is done in model pipline (train.py)
 
 def get_X_and_y(df) :
-    X = df.drop(["Conversion"] , axis=1)
+    X = df.drop(["Conversion" , "ConversionRate"] , axis=1)
     y = df["Conversion"]
     return X , y
--- src/train.py
+++ src/train.py
@@ -3,6 +3,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder , StandardScaler
+from xgboost import XGBClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.metrics import f1_score , classification_report
+
 
 df = pd.read_csv(r"C:\Users\sailj\OneDrive\文档\GitHub\Ad Conversion Model\dataset\digital_marketing_campaign_dataset.csv")
 X , y = get_X_and_y(df)
@@ -12,8 +17,33 @@ num_cols = X.select_dtypes(include=["number"]).columns
 
 X_train , X_test , y_train , y_test = train_test_split(X , y , test_size=0.2 , random_state=42 , stratify=y)
 
+weights = compute_sample_weight(
+    class_weight="balanced" ,
+    y = y_train
+)
+
 preprocessing = ColumnTransformer([
     ("cat" , OneHotEncoder(handle_unknown="ignore") , cat_cols) ,
     ("num" , StandardScaler() , num_cols)
 ])
 
+pipeline = Pipeline([
+    ("preprocess" , preprocessing) ,
+    ("xgbc" , XGBClassifier(
+        n_jobs = -1,
+        random_state = 42 ,
+        subsample = 0.8 ,
+        colsample_bytree = 0.8 ,
+    ))
+])
+
+pipeline.fit(
+    X_train ,
+    y_train,
+    xgbc__sample_weight = weights
+)
+
+y_pred = pipeline.predict(X_test)
+
+print(f1_score(y_test , y_pred))
+print(classification_report(y_test, y_pred))
